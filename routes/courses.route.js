53131f12b6a658b608ef52473ae1128076b823a4
wensespl <wpenadillol@uni.pe>
2021-11-21 19:54:53 -0500

    Update courses.route.js

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  addCourse,
+  createCourse,
+  deleteCourse,
   getAllCourses,
   getCourse,
 } = require("../controllers/courses.controller");
 
-router.route("/").get(getAllCourses).post(addCourse);
-router.route("/:courseId").get(getCourse);
+router.route("/").get(getAllCourses).post(createCourse);
+router.route("/:courseId").get(getCourse).delete(deleteCourse);
 
 module.exports = router;
