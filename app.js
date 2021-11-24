002fb18e0910494599e9ff21898b301dfe66973a
wensespl <wpenadillol@uni.pe>
2021-11-23 21:07:33 -0500

    Update app.js
    Activando el uso de CORS para admitir peticiones del Frontend

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const courses = require("./routes/courses.route");
 const alumnos = require("./routes/alumnos.route");
-const tutores = reuire("./routes/tutores.route");
+const tutores = require("./routes/tutores.route");
+const auth = require("./routes/auth.route");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // routes
+app.use("/api/v1/auth", auth);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/alumnos", alumnos);
 app.use("/api/v1/tutores", tutores);
@@ -25,4 +33,4 @@ const start = async () => {
   }
 };
 
-start()
+start();
