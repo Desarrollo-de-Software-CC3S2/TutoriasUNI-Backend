3378edd612c80d28f88310996b11977b12048930
wensespl <wpenadillol@uni.pe>
2021-12-04 22:33:16 -0500

    Funcionalidad de cursos
    Añadiendo funcionalidad a los controladores de cursos.

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -3,7 +3,7 @@ const Course = require("../models/Course.model");
 const createCourse = async (req, res) => {
   try {
     const course = await Course.create(req.body);
-    res.status(201).json({ course });
+    res.status(201).json(course);
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -63,6 +63,7 @@ const getAllCourses = async (req, res) => {
 module.exports = {
   createCourse,
   deleteCourse,
+  updateCourse,
   getAllCourses,
   getCourse,
 };
--- models/Course.model.js
+++ models/Course.model.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+const short = require("shortid");
 
 const CourseSchema = new mongoose.Schema({
   nombre: { type: String, required: true, trim: true, maxlength: 25 },
   tema: { type: String, required: true, trim: true, maxlength: 25 },
-  codigo: { type: String, required: true, trim: true, maxlength: 10 },
+  codigo: {
+    type: String,
+    trim: true,
+    maxlength: 10,
+    default: short.generate(),
+  },
   profesorId: { type: String, required: true },
   contenido: { type: [mongoose.Schema.Types.Mixed], default: [] },
 });
