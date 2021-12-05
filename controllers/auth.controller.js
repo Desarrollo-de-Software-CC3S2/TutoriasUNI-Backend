d055b52d5c6dc3d4b393d3551f416c87864a6184
wensespl <wpenadillol@uni.pe>
2021-12-04 22:32:03 -0500

    Update auth.controller.js
    Modificando login para admitir tutores

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,16 @@
 const Alumno = require("../models/Alumno.model");
+const Tutor = require("../models/Tutor.model");
+const bcrypt = require("bcrypt");
 
 const login = async (req, res) => {
   try {
     const { email: email, password: password } = req.params;
-    const user = await Alumno.findOne({ email: email, password: password });
-    if (!user) {
+    let user = await Alumno.findOne({ email: email });
+    if(!user) {
+      user = await Tutor.findOne({ email: email });
+    }
+    const exist = await bcrypt.compare(password, user.password);
+    if (!exist) {
       return res.status(404).json({ msg: "Unregistered user" });
     }
     res.status(200).json(user);
@@ -15,8 +21,17 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    const user = await Alumno.create(req.body);
-    res.status(201).json(user);
+    const exist = await Alumno.findOne({ email: req.body.email });
+    if (!exist) {
+      let passwordHash = await bcrypt.hash(req.body.password,10);
+      req.body.password = passwordHash;
+      const user = await Alumno.create(req.body);
+      res.status(201).json(user);
+    } else {
+      res
+        .status(400)
+        .json({ msg: `User with email: ${req.body.email} already exists` });
+    }
   } catch (error) {
     res.status(500).json({ msg: error });
   }
