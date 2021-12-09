a8b32b261e2bc8a27db367838a349a1f0efb4891
wensespl <wpenadillol@uni.pe>
2021-12-09 00:56:44 -0500

    Cambios menores

--- routes/bot.route.js
+++ routes/bot.route.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    mensaje_bot,
-  } = require("../controllers/bot.controller");
+const { mensaje_bot } = require("../controllers/bot.controller");
 
 router.route("/").post(mensaje_bot);
-
 
 module.exports = router;
