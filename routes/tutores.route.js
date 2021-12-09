8f3bf3297ed04873da674a84f2a93002b158cef4
wensespl <wpenadillol@uni.pe>
2021-12-08 23:36:53 -0500

    Update tutores.route.js

--- routes/tutores.route.js
+++ routes/tutores.route.js
@@ -10,8 +10,8 @@ const {
   updateUser,
 } = require("../controllers/tutores.controller");
 
-router.route("/").get(getAllUsers).post(createUser).patch(updateUser);
-router.route("/:userId").get(getUser);
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:userId").get(getUser).patch(updateUser);
 router.route("/:userId/courses").get(getUserAllCourses);
 router.route("/:userId/courses/:courseId").get(getUserCourse);
 
