9b69b7468701c5b9b7c69ddb4cf23f7ddec9cb6b
wensespl <wpenadillol@uni.pe>
2021-12-11 22:43:41 -0500

    Modificando actualizar y eliminar curso
    añadiendo la funcionalidad de eliminar y actualizar cursos

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -6,9 +6,10 @@ const {
   deleteCourse,
   getAllCourses,
   getCourse,
+  updateCourse,
 } = require("../controllers/courses.controller");
 
 router.route("/").get(getAllCourses).post(createCourse);
-router.route("/:courseId").get(getCourse).delete(deleteCourse);
+router.route("/:courseId").get(getCourse).delete(deleteCourse).patch(updateCourse);
 
 module.exports = router;
