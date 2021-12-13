7fdcb10622db7070b0ceeda906afa6cebad29d06
wensespl <wpenadillol@uni.pe>
2021-12-12 19:41:05 -0500

    Update bot.route.js

--- routes/bot.route.js
+++ routes/bot.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { mensaje_bot } = require("../controllers/bot.controller");
+const {
+  mensaje_bot,
+  guardar_mensaje,
+} = require("../controllers/bot.controller");
 
 router.route("/").post(mensaje_bot);
+router.route("/:userId").patch(guardar_mensaje);
 
 module.exports = router;
